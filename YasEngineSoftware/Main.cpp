20bf5b65a0c24df637a989a871c5fb40c7aaba90
Luke Sawicki <lukaszsa1@gmail.com>
2022-05-11 17:48:58 +0200

    Working ok. Only sdl simple project, for now.

--- YasEngineSoftware/Main.cpp
+++ YasEngineSoftware/Main.cpp
@@ -1,7 +1,7 @@
 //#include<Windows.h>
 #define SDL_MAIN_HANDLED
-#include <SDL.h>
-#include <stdio.h>
+#include<SDL.h>
+#include<stdio.h>
 #include<cstdlib>
 #include<iostream>
 #include<vector>
@@ -19,12 +19,11 @@
 #include"Player.hpp"
 #include"InputOutputHandler.hpp"
 
-
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("YasEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
@@ -39,9 +38,9 @@ int main()
     SDL_RenderSetLogicalSize(renderer, width, height);
     SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
 
-    SDL_Texture* screen_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+    SDL_Texture* screenTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width, height);
 
-    unsigned int* pixels = new unsigned int[width * height * 4];
+    Uint32* pixelsTable = new Uint32[width * height * 4];
 
     while (1)
     {
@@ -51,21 +50,17 @@ int main()
             if (event.type == SDL_QUIT) exit(0);
         }
 
-        // Set every pixel to white.
         for (int y = 0; y < 22; ++y)
         {
             for (int x = 0; x < 33; ++x)
             {
-                pixels[x + y * width] = 0xffffffff;
+                pixelsTable[x + y * width] = 0xffffffff;
             }
         }
 
-        // It's a good idea to clear the screen every frame,
-        // as artifacts may occur if the window overlaps with
-        // other windows or transparent overlays.
         SDL_RenderClear(renderer);
-        SDL_UpdateTexture(screen_texture, NULL, pixels, width * 4);
-        SDL_RenderCopy(renderer, screen_texture, NULL, NULL);
+        SDL_UpdateTexture(screenTexture, NULL, pixelsTable, width * 4);
+        SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 }
