cf3f4aa381872e9606128fc71ed7ab92da06bb08
lukesawicki <lukaszsa.1@gmail.com>
2024-08-24 09:04:15 +0200

    Deleted code file because git error with renaming files.

--- YasEngine/Randomizer.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOMIZER_HPP
-#define RANDOMIZER_HPP
-
-class Randomizer {
- public:
-  static int drawNumberClosedInterval(int min, int max);
-};
-
-#endif
--- YasEngine/line_segment.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef LINESEGMENT_HPP
-#define LINESEGMENT_HPP
-
-struct LineSegment {
-  Vector2D<float> point_0;
-  Vector2D<float> point_1;
-};
-
-#endif
